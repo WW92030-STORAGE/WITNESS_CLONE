5d50b2b23b0013a09da845ce96f18ac58bf53aa6
WW92030 <65929722+WW92030-STORAGE@users.noreply.github.com>
2023-06-15 22:01:03 -0500

    Create miscsymbols.h

---
+++ miscsymbols.h
@@ -0,0 +1,50 @@
+class Endpoint : public Object {
+    public:
+    bool starting;
+    Endpoint(bool s) : Object() {
+        starting = s;
+    }
+};
+
+class Dot : public Object {
+    public:
+    Dot() : Object() {
+        
+    }
+};
+
+class Blob : public Object {
+    public:
+    Blob() : Object() {
+        color = BLACK;
+    }
+    
+    Blob(Color c) : Object() {
+        color = c;
+    }
+};
+
+class Star : public Object {
+    public:
+    Star() : Object() {
+        color = WHITE;
+    }
+    
+    Star(Color c) : Object() {
+        color = c;
+    }
+};
+
+class Triangle : public Object {
+    public:
+    int x;
+    Triangle(int n) : Object() {
+        x = n;
+        color = ORANGE;
+    }
+    
+    Triangle(int n, Color c) : Object() {
+        x = n;
+        color = c;
+    }
+};
