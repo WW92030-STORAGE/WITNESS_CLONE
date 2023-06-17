7c150dbcb5ef92d5e24defad487ffb625c07adbc
WW92030 <65929722+WW92030-STORAGE@users.noreply.github.com>
2023-06-17 12:20:27 -0500

    Update and rename example.cpp to examples/example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -65,6 +65,7 @@ int main()
     
     // EXAMPLE III
     // https://windmill.thefifthmatt.com/build/CAkSAigIEggIBBoECAIQABICKAMSAggKEgASBAgLMAESAigfEgwICSIICAISBAEBAQESABICCAoSAigHEgIIBhICKAcSAggKEgASCwgJIgcIAxIDAQEBEgIoBRICCAMSAigI_0
+    // https://windmill.thefifthmatt.com/build/CAkSAigIEggIBBoECAIQABICKCUSDAgJIggIAhIEAQEBARIAEgIIChICKBESCwgJIgcIAxIDAQEBEgIoBRICCAMSAigI_0
     
     v = vector<vector<Object*>>(9, vector<Object*>(9));
     for (int i = 0; i < 9; i++) {
@@ -74,13 +75,13 @@ int main()
     v[8][0] = new Endpoint(true);
     v[0][8] = new Endpoint(false);
     v[5][1] = new BlockGroup(1, 0, vector<pii>({{0, 0}, {0, 1}, {1, 0}, {1, 1}}));
-    v[7][3] = new BlockGroup(1, 0, vector<pii>({{0, 0}, {0, 1}, {0, 2}}));
-    v[6][2] = new Dot();
+    v[7][3] = new BlockGroup(1, 0, vector<pii>({{0, 0}, {1, 0}, {2, 0}}));
+    // v[6][2] = new Dot();
     v[5][3] = new Cancel();
-    v[7][1] = new Cancel();
+    // v[7][1] = new Cancel();
     
-    v[1][3] = new Cancel();
-    v[1][5] = new Triangle(1);
+    // v[1][3] = new Cancel();
+    // v[1][5] = new Triangle(1);
     
     Grid grid3 = Grid(v);
     grid3.defaultGrid();
@@ -115,8 +116,6 @@ int main()
     sx.solve();
     sx.disp();
     cout << "END OF EXAMPLE IV\n";
-    
-    /*
     
     // EXAMPLE V
     // https://windmill.thefifthmatt.com/build/CAkSAigHEgIIBRIICAQaBAgCEAASAigCEgIIBRIAEgIIBRICKAUSAggFEgIoAxICCAUSAigFEgIIBRICKAMSAggFEgASAggFEgASAggFEgIoBRICCAUSAigFEgIIBRICKAcSAggFEgIoAxICCAUSABICCAUSABICCAUSAigGEgIIAxICKAg=_0
@@ -154,8 +153,6 @@ int main()
     sx.solve();
     sx.disp();
     
-    */
-    
     // EXAMPLE VI
     // https://windmill.thefifthmatt.com/build/CAkSAigIEggIBBoECAIQABICKBUSDAgJIggIAhIEAQEBABICKAMSDAgJIggIAhIEAQEAARICKAYSAggFEgIoDxICCAUSAigOEgIIAxICKAg=_0
     
@@ -178,6 +175,90 @@ int main()
     Grid grid6 = Grid(v);
     
     sx.set(grid6);
+    sx.solve();
+    sx.disp();
+    
+    // EXAMPLE VII
+    // https://windmill.thefifthmatt.com/build/CAkSAggGEgIoBxIICAQaBAgCEAASAigCEgIIBRIECAgQARICKAMSBAgHEAISAigDEgIIBhICKAcSBAgLMAISAigDEgQIBxACEgASBAgIEAESAigFEgIIBhICKAUSBAgIEAISABIECAcQARIAEgQICzACEgIIBRICKAgSAggGEgIoAxIECAcQARIAEgQICzABEgIIBRIECAgQAhICKAMSAggDEgIoBxICCAY=_0
+    
+    v = vector<vector<Object*>>(9, vector<Object*>(9));
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+          v[i][j] = new Object();
+          if (i % 2 == 0 || j % 2 == 0) v[i][j]->isPath = true;
+        }
+    }
+    
+    v[8][0] = new Endpoint(true);
+    v[0][8] = new Endpoint(false);
+    v[0][0] = new Dot();
+    v[2][2] = new Dot();
+    v[4][4] = new Dot();
+    v[6][6] = new Dot();
+    v[8][8] = new Dot();
+    v[3][1] = new Triangle(2);
+    v[5][5] = new Triangle(2);
+    v[7][3] = new Triangle(1);
+    v[7][1] = new Blob(RED);
+    v[5][3] = new Blob(RED);
+    v[1][7] = new Blob(CYAN);
+    v[3][5] = new Blob(CYAN);
+
+    v[1][3] = new Star(RED);
+    v[3][7] = new Star(RED);
+    v[5][1] = new Star(CYAN);
+    v[7][5] = new Star(CYAN);
+    v[1][2]->isPath = false;
+    v[5][6]->isPath = false;
+    v[7][4]->isPath = false;
+    
+    Grid grid7 = Grid(v);
+    
+    sx.set(grid7);
+    sx.solve();
+    sx.disp();
+    
+    // EXAMPLE VIII
+    // https://windmill.thefifthmatt.com/cmkhg78
+    
+    v = vector<vector<Object*>>(11, vector<Object*>(11));
+    for (int i = 0; i < 11; i++) {
+        for (int j = 0; j < 11; j++) v[i][j] = new Object();
+    }
+    
+    v[10][0] = new Endpoint(true);
+    v[0][10] = new Endpoint(false);
+    
+    v[1][1] = new Triangle(1);
+    v[3][1] = new Triangle(1);
+    v[5][1] = new Triangle(2);
+    v[7][1] = new Triangle(2);
+    v[9][1] = new Triangle(2);
+    v[1][3] = new Triangle(3);
+    v[3][3] = new Triangle(2);
+    v[5][3] = new Triangle(2);
+    v[7][3] = new Triangle(2);
+    v[9][3] = new Triangle(3);
+    v[1][5] = new Triangle(1);
+    v[3][5] = new Triangle(1);
+    v[5][5] = new Triangle(2);
+    v[7][5] = new Triangle(2);
+    v[9][5] = new Triangle(2);
+    v[1][7] = new Triangle(1);
+    v[3][7] = new Triangle(1);
+    v[5][7] = new Triangle(3);
+    v[7][7] = new Triangle(2);
+    v[9][7] = new Triangle(2);
+    v[1][9] = new Triangle(3);
+    v[3][9] = new Triangle(2);
+    v[5][9] = new Triangle(2);
+    v[7][9] = new Triangle(2);
+    v[9][9] = new Triangle(3);
+    
+    Grid grid8 = Grid(v);
+    grid8.defaultGrid();
+    
+    sx.set(grid8);
     sx.solve();
     sx.disp();
     
