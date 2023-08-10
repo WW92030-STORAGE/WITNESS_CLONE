105964ebe8079fe8056c8e2072a408065fa369c3
WW92030 <65929722+WW92030-STORAGE@users.noreply.github.com>
2023-08-09 19:41:30 -0700

    Add files via upload

--- PYTHON/miscsymbols.py
+++ PYTHON/miscsymbols.py
@@ -1,31 +1,31 @@
 from object import Object, Color
 
 class Endpoint(Object):
-    starting = False
-    def __init__(self, start):
-        self.starting = start
-        self.isPath = True
+	starting = False
+	def __init__(self, start):
+		self.starting = start
+		self.isPath = True
 
 class Dot(Object):
-    def __init__(self):
-        self.isPath = True
+	def __init__(self):
+		self.isPath = True
 
 class Blob(Object):
-    def __init__(self, col = Color.BLACK):
-        self.color = col
-        
+	def __init__(self, col = Color.BLACK):
+		self.color = col
+		
 class Star(Object):
-    def __init__(self, col = Color.WHITE):
-        self.color = col
+	def __init__(self, col = Color.WHITE):
+		self.color = col
 
 class Triangle(Object):
-    x = 1
-    def __init__(self, num, col = Color.ORANGE):
-        self.color = col
-        self.x = num
+	x = 1
+	def __init__(self, num, col = Color.ORANGE):
+		self.color = col
+		self.x = num
 
 class Cancel(Object):
-    ignored = False
-    def __init__(self):
-        self.color = Color.NIL
-        self.ignored = False
+	ignored = False
+	def __init__(self):
+		self.color = Color.NIL
+		self.ignored = False
