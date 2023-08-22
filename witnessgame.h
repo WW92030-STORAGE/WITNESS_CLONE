adb7c25bdd6201a09730c036eb0510fdb2892b96
WW92030 <65929722+WW92030-STORAGE@users.noreply.github.com>
2023-08-22 09:57:17 -0700

    Add files via upload

---
+++ witnessgame.h
@@ -0,0 +1,36 @@
+#ifndef WITNESSGAME_H
+#define WITNESSGAME_H
+
+#include <vector>
+#include <map>
+
+#include "object.h"
+#include "miscsymbols.h"
+#include "blockgroup.h"
+#include "util.h"
+#include "grid.h"
+#include "solver.h"
+#include "randgrid.h"
+
+class WitnessGame {
+    public:
+    Grid grid;
+    std::vector<std::pair<int, int>> startpts;
+    std::pair<int, int> origin;
+    std::pair<int, int> pos;
+    std::map<std::pair<int, int>, std::pair<int, int>> parent;
+    
+    const int dy[4] = {01, 00, -1, 00};
+    const int dx[4] = {00, 01, 00, -1};
+    const char dz[4] = {'R', 'D', 'L', 'U'};
+    
+    WitnessGame(Grid g); // Init the grid
+    void reset(Grid g); // Change the grid and reset the game
+    void setStart(int x); // Set the starting point to an available one
+    
+    void processInput(char c); // Char is converted to direction based on dz
+    bool reachedEnd(); // Have we reached an end?
+    void clear(); // Clear the drawn path from the grid
+};
+
+#endif
